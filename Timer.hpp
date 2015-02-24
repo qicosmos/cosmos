fcd614349bac9c23062177a6a28b3c2599b00dc5
qicosmos <qicosmos@163.com>
2015-02-24 16:56:18 +0800

    Create Timer.hpp

---
+++ Timer.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include<chrono>
+using namespace std;
+using namespace std::chrono;
+
+class Timer
+{
+public:
+	Timer() : m_begin(high_resolution_clock::now()) {}
+	void reset() { m_begin = high_resolution_clock::now(); }
+
+	////默认输出毫秒
+	//int64_t elapsed() const
+	//{
+	//	return duration_cast<chrono::milliseconds>(high_resolution_clock::now() - m_begin).count();
+	//}
+
+	//默认输出秒
+	double elapsed() const
+	{
+		return duration_cast<duration<double>>(high_resolution_clock::now() - m_begin).count();
+	}
+
+	//微秒
+	int64_t elapsed_micro() const
+	{
+		return duration_cast<chrono::microseconds>(high_resolution_clock::now() - m_begin).count();
+	}
+
+	//纳秒
+	int64_t elapsed_nano() const
+	{
+		return duration_cast<chrono::nanoseconds>(high_resolution_clock::now() - m_begin).count();
+	}
+
+	////秒
+	//int64_t elapsed_seconds() const
+	//{
+	//	return duration_cast<chrono::seconds>(high_resolution_clock::now() - m_begin).count();
+	//}
+
+	//分
+	int64_t elapsed_minutes() const
+	{
+		return duration_cast<chrono::minutes>(high_resolution_clock::now() - m_begin).count();
+	}
+
+	//时
+	int64_t elapsed_hours() const
+	{
+		return duration_cast<chrono::hours>(high_resolution_clock::now() - m_begin).count();
+	}
+
+private:
+	time_point<high_resolution_clock> m_begin;
+};
+
