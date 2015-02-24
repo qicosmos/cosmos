387316b73413782d8f10bda3aab6d8e1521bb5f8
qicosmos <qicosmos@163.com>
2015-02-24 16:51:45 +0800

    Create ScopeGuard.hpp

---
+++ ScopeGuard.hpp
@@ -0,0 +1,36 @@
+template <typename F>
+class ScopeGuard
+{
+public:
+	explicit ScopeGuard(F && f) : m_func(std::move(f)), m_dismiss(false){}
+	explicit ScopeGuard(const F& f) : m_func(f), m_dismiss(false){}
+
+	~ScopeGuard()
+	{
+		if (!m_dismiss&&m_func != nullptr)
+			m_func();
+	}
+
+	ScopeGuard(ScopeGuard && rhs) : m_func(std::move(rhs.m_func)), m_dismiss(rhs.m_dismiss)
+	{
+		rhs.Dismiss();
+	}
+
+	void Dismiss()
+	{
+		m_dismiss = true;
+	}
+
+private:
+	F m_func;
+	bool m_dismiss;
+
+	ScopeGuard();
+	ScopeGuard(const ScopeGuard&);
+	ScopeGuard&operator=(const ScopeGuard&);
+};
+
+template <typename F>ScopeGuard<typename std::decay<F>::type> MakeGuard(F && f)
+{ 
+	return ScopeGuard<typename std::decay<F>::type>(std::forward<F>(f)); 
+}
