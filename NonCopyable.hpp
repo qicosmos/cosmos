1c20123476a7773ffa67032187c4cb530362903e
qicosmos <qicosmos@163.com>
2015-02-24 16:58:23 +0800

    Create NonCopyable.hpp

---
+++ NonCopyable.hpp
@@ -0,0 +1,8 @@
+#pragma once
+class NonCopyable
+{
+public:
+	NonCopyable(const NonCopyable&) = delete; // deleted
+	NonCopyable& operator = (const NonCopyable&) = delete; // deleted
+	NonCopyable() = default;   // available
+};
