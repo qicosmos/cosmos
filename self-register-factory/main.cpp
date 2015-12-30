683a11ec42f4955f5bb5e9da5d53b98efea72c8e
qicosmos <qicosmos@163.com>
2015-12-30 14:43:22 +0800

    Create main.cpp

---
+++ self-register-factory/main.cpp
@@ -0,0 +1,11 @@
+#include "Message1.hpp"
+#include "Message2.hpp"
+
+int main()
+{
+	Message* p = factory::produce("message1");
+	p->foo();   //Message1
+	
+	auto p2 = factory::produce_unique("message1");
+	p2->foo();
+}
